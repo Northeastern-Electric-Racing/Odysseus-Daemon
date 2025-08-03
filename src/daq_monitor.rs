73fde4fc9175f040fa7d47d5f8cec0f387f54ca8
Jack Rubacha <rubacha.jack03@gmail.com>
2025-08-03 16:53:07 -0400

    spawn begin

--- src/daq_monitor.rs
+++ src/daq_monitor.rs
@@ -19,22 +19,26 @@ pub async fn monitor_daq(
     mqtt_sender_tx: Sender<PublishableMessage>,
     can_handler_tx: Sender<CanFrame>,
 ) {
-    let mut timeout = interval(Duration::from_millis(10000));
+    let mut timeout = interval(Duration::from_millis(1000));
 
     let mut watchdog = false;
 
     let (daq_monitor_tx, mut daq_monitor_rx) = tokio::sync::mpsc::channel::<bool>(1000);
     let daq_cancel_token = CancellationToken::new();
 
-    let mut task: Option<JoinHandle<()>> = None;
+    let mut task: JoinHandle<()> = tokio::task::spawn(collect_daq(
+        daq_cancel_token.clone(),
+        device.clone(),
+        daq_monitor_tx.clone(),
+        mqtt_sender_tx.clone(),
+        can_handler_tx.clone(),
+    ));
 
     loop {
         tokio::select! {
             _ = cancel_token.cancelled() => {
                 daq_cancel_token.cancel();
-                if task.is_some() {
-                task.unwrap().await;
-                }
+                task.await;
                 debug!("Shutting down daq monitor");
                 break;
             }
@@ -42,11 +46,8 @@ pub async fn monitor_daq(
             _ = timeout.tick() => {
                 if !watchdog {
                     daq_cancel_token.cancel();
-                    if task.is_some() {
-                        task.unwrap().await;
-                        task = None;
-                    }
-                    task = Some(tokio::task::spawn(collect_daq(daq_cancel_token.clone(), device.clone(), daq_monitor_tx.clone(), mqtt_sender_tx.clone(), can_handler_tx.clone())));
+                    task.await;
+                    task = tokio::task::spawn(collect_daq(daq_cancel_token.clone(), device.clone(), daq_monitor_tx.clone(), mqtt_sender_tx.clone(), can_handler_tx.clone()));
                     warn!("Respawing DAQ thread");
                 }
                 watchdog = false;
